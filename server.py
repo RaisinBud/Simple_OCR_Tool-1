100ba3e1b0aed2c6d18690950698defff6799ab2
RaisinBud <preeti26.m@gmail.com>
2024-02-24 15:09:54 +0530

    Initial commit

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.templating import Jinja2Templates
 import shutil
 import ocr
+#import ocrpython --version
 import os
 import uuid
 import json
@@ -9,35 +10,15 @@ import json
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home(request: Request):
+@app.get("/ocr/config/")
+def Config(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/api/v1/extract_text")
-async def extract_text(image: UploadFile = File(...)):
+@app.post("/ocr/get-texts-for-images/")
+async def Get_Texts_For_Images(image: UploadFile = File(...)):
     temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
     text = await ocr.read_image(temp_file)
     return {"filename": image.filename, "text": text}
-
-@app.post("/api/v1/bulk_extract_text")
-async def bulk_extract_text(request: Request, bg_task: BackgroundTasks):
-    images = await request.form()
-    folder_name = str(uuid.uuid4())
-    os.mkdir(folder_name)
-
-    for image in images.values():
-        temp_file = _save_file_to_disk(image, path=folder_name, save_as=image.filename)
-
-    bg_task.add_task(ocr.read_images_from_dir, folder_name, write_to_file=True)
-    return {"task_id": folder_name, "num_files": len(images)}
-
-@app.get("/api/v1/bulk_output/{task_id}")
-async def bulk_output(task_id):
-    text_map = {}
-    for file_ in os.listdir(task_id):
-        if file_.endswith("txt"):
-            text_map[file_] = open(os.path.join(task_id, file_)).read()
-    return {"task_id": task_id, "output": text_map}
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
     extension = os.path.splitext(uploaded_file.filename)[-1]
